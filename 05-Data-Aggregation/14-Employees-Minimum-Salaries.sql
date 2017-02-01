ba406b164721e8ee552cb9c673e9cf5885bacd2d
Anna <annsta@gmail.com>
2017-02-01 03:48:31 +0200

    Data Aggregation

---
+++ 05-Data-Aggregation/14-Employees-Minimum-Salaries.sql
@@ -0,0 +1,5 @@
+SELECT
+  DepartmentID, MIN(Salary) AS MinimumSalary
+FROM Employees
+WHERE DepartmentID IN (2,5,7) AND HireDate > '2000/01/01'
+GROUP BY DepartmentID
---
+++ 05-Data-Aggregation/17-Employees-Count-Salaries.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(Salary) AS Count
+FROM Employees
+WHERE ManagerID IS NULL
