175569705c71505be13afa6c8046bd266072abfd
Anna <annsta@gmail.com>
2017-02-03 01:14:25 +0200

    Data Aggregation

--- 05-Data-Aggregation/18-3rd-Highest-Salary.sql
+++ 05-Data-Aggregation/18-3rd-Highest-Salary.sql
@@ -11,3 +11,12 @@ WHERE
    ORDER BY Salary DESC 
    OFFSET 2 ROWS FETCH NEXT 1 ROWS ONLY) IS NOT NULL
 GROUP BY DepartmentID
+
+-- solution with DENSE_RANK
+SELECT DepartmentID, Salary AS ThirdHighestSalary
+FROM 
+  (SELECT DepartmentID, Salary,
+     DENSE_RANK() OVER (PARTITION BY DepartmentId ORDER BY Salary DESC) AS Rank
+   FROM Employees
+   GROUP BY DepartmentID, Salary) AS RankedSalaries
+WHERE RankedSalaries.Rank = 3
--- 05-Data-Aggregation/19-Salary-Challenge.sql
+++ 05-Data-Aggregation/19-Salary-Challenge.sql
@@ -9,6 +9,17 @@ ON e.DepartmentID = av.DepartmentID
 WHERE Salary > AverageSalary
 ORDER BY e.DepartmentID
 
+-- another solution
+SELECT TOP 10
+  FirstName, LastName, DepartmentID
+FROM Employees AS emp1
+WHERE Salary >
+  (SELECT AVG(Salary)
+   FROM Employees AS emp2
+   WHERE emp1.DepartmentID = emp2.DepartmentID
+   GROUP BY DepartmentID)
+ORDER BY DepartmentID
+
 -- how come a different output ?!
 SELECT
   DepartmentID, AVG(Salary) AS AverageSalary
