7042c59671643be27406c95dc19686bc1f9a38ec
Anna <annsta@gmail.com>
2017-02-12 16:31:12 +0200

    Functions Triggers And Transactions

--- 08-Functions-Triggers-And-Transactions/18-Create-Table-Emails.sql
+++ 08-Functions-Triggers-And-Transactions/18-Create-Table-Emails.sql
@@ -25,7 +25,7 @@ AS
 BEGIN  
   INSERT INTO NotificationEmails (Recipient, Subject, Body) 
   (SELECT AccountId, 
-	 CONCAT('Balance change for account: ', AccountId),
+     CONCAT('Balance change for account: ', AccountId),
      CONCAT('On ', GETDATE(), ' your balance was changed from ', OldSum, ' to ', NewSum, '.')
    FROM inserted)
 END
