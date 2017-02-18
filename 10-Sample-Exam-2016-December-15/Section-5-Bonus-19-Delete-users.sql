5383d89af233fcd30a70249698644c5a4a0e99c4
Anna <annsta@gmail.com>
2017-02-18 14:45:07 +0200

    Exam Prep

---
+++ 10-Sample-Exam-2016-December-15/Section-5-Bonus-19-Delete-users.sql
@@ -0,0 +1,28 @@
+CREATE TRIGGER tr_DeleteUser ON Users 
+INSTEAD OF DELETE
+AS
+BEGIN  
+  -- remove NOT NULL constraint for UserId in Messages
+  ALTER TABLE Messages
+  ALTER COLUMN UserId int NULL  
+
+  -- set UserId in Messages to NULL for all users to be deleted
+  UPDATE Messages
+  SET UserId = NULL  
+  WHERE UserId IN (SELECT Id FROM deleted)  
+
+  -- delete user-chat entries in mapping table UsesChats for all users to be deleted
+  DELETE FROM UsersChats
+  WHERE UserId IN (SELECT Id FROM deleted)  
+
+  -- delete users
+  DELETE FROM Users
+  WHERE Users.Id = (SELECT Id FROM deleted)  
+END
+
+GO
+--testing
+DELETE FROM Users WHERE Users.Id = 1
+
+SELECT * FROM Users WHERE Id = 1
+SELECT * FROM Messages WHERE UserId = 1
